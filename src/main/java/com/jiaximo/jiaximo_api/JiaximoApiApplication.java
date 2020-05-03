ca0c51fdb02340592d9df117f8bbd2839e44e6f2
chxie <chxie@haivera.com>
2020-05-03 18:11:17 +0800

    初始化项目

---
+++ src/main/java/com/jiaximo/jiaximo_api/JiaximoApiApplication.java
@@ -0,0 +1,13 @@
+package com.jiaximo.jiaximo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiaximoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiaximoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiaximo/jiaximo_api/JiaximoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiaximo.jiaximo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiaximoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
